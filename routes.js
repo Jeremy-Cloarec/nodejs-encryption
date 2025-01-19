4dfa9a0745e9a50e17a12f21f833971b08889167
Jeremy Cloarec <jeremycloarec@msn.com>
2025-01-19 12:17:04 +0100

    first commit

---
+++ routes.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { encryptData, decryptData } from './encryption.js'
+
+const router = express.Router()
+
+router.post('/encrypt', (req, res) => {
+    const { data } = req.body
+    const encryptedData = encryptData(data)
+    res.json({ encryptedData })
+})
+
+router.post('/decrypt', (req, res) => {
+    console.log(req.body)
+    const { encryptedData } = req.body
+    const data = decryptData(encryptedData)
+    res.json({ data })
+})
+
+export default router
